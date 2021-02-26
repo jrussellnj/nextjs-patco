7c34aa3209b9cacc82da50c52c03f640e89965f2
Joseph Russell <116615+jrussellnj@users.noreply.github.com>
2021-02-26 11:55:17 -0500

    Adding in station page functionality to get stop times

--- pages/[station_name].js
+++ pages/[station_name].js
@@ -1,12 +1,31 @@
 import { useRouter } from 'next/router'
 
-const Post = () => {
+function Station({ data }) {
   const router = useRouter()
   const { station_name } = router.query
 
   // Make call to /get-stop-times/{ station_name} here!
+  // console.log('/get-stop-times/' + station_name)
 
-  return <p>station name: { station_name }</p>
+  console.log("--data", data)
+  return (
+    <>
+      <h1>{ station_name }</h1>
+
+      <div>{ data }</div>
+    </>
+  )
 }
 
-export default Post
+// Get the departures for the given station
+export async function getServerSideProps(context) {
+
+  // Fetch data from external API
+  const res = await fetch(`http://localhost:3000/get-stop-times/${context.params.station_name}`)
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
+
+export default Station
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link'
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    <div>
+      Welcome to Next.js!
+      <Link href="/collingswood">
+        <a>Collingswood</a>
+      </Link>
+    </div>
+  )
 }
 
 export default HomePage
